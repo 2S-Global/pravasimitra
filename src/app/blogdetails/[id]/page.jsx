ce7cf903bd4364fcd43a29c12bbf4d8b8b7bdf57
2S-Global <2sglobaltechnologies@gmail.com>
2025-06-18 13:13:49 +0530

    new update

--- src/app/blogdetails/[id]/page.jsx
+++ src/app/blogdetails/[id]/page.jsx
@@ -8,30 +8,37 @@ export default function BlogDetails({ params }) {
   const blog = blogs.find((b) => b.id === parseInt(params.id));
 
 
-  return (
+ return (
     <>
       <Header />
       <OtherBanner page_title={blog.title} />
       <div className="tm-section bg-white tm-padding-section">
         <div className="container">
-          <div className="row">
-            <div className="col-lg-6">
-              <h2>{blog.title}</h2>
-              {/* <p><strong>Author:</strong> {blog.userId} | <strong>Date:</strong> {blog.date}</p> */}
-              {blog.description.split("\n\n").map((para, index) => (
-                <p
-                  key={index}
-                  style={{ textAlign: "justify", marginBottom: "1em" }}
-                  dangerouslySetInnerHTML={{ __html: para.trim() }}
-                />
-              ))}
-            </div>
-           <div className="col-lg-6">
+          <div className="row gy-5 align-items-start">
+            {/* Blog Image (on top in mobile view) */}
+            <div className="col-12 col-lg-6 order-lg-2">
               <img
                 src={blog.image}
                 alt={blog.title}
-                style={{ width: "100%", borderRadius: "10px", boxShadow: "0 0 10px rgba(0,0,0,0.1)" }}
+                className="img-fluid rounded shadow"
+                style={{  objectFit: "cover", width: "100%" }}
               />
+              {/* <div className="mt-3 text-muted small">
+                <i className="bi bi-person-fill"></i> <strong>{blog.userId}</strong> &nbsp;|&nbsp;
+                <i className="bi bi-calendar-event"></i> {blog.date}
+              </div> */}
+            </div>
+
+            {/* Blog Content */}
+            <div className="col-12 col-lg-6 order-lg-1">
+              <h2 className="mb-4">{blog.title}</h2>
+              {blog.description.split("\n\n").map((para, index) => (
+                <p
+                  key={index}
+                  style={{ textAlign: "justify", marginBottom: "1em", lineHeight: "1.8", fontSize: "1.05rem" }}
+                  dangerouslySetInnerHTML={{ __html: para.trim() }}
+                />
+              ))}
             </div>
           </div>
         </div>
