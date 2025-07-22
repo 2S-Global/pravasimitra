fef9d230f5221a54f502d76f9b569ae5b068f454
souvik-03-sd <souvik.2sglobal@gmail.com>
2025-07-22 12:17:15 +0530

    update new

--- src/app/api/marketplace/details/route.js
+++ src/app/api/marketplace/details/route.js
@@ -47,6 +47,10 @@ export const GET = async (req) => {
       return addCorsHeaders(NextResponse.json({ msg: "Item not found" }, { status: 404 }));
     }
 
+    item.images = item.images.map(filename => {
+      return `${process.env.IMAGE_URL}/e-marketplace/${filename}`;
+    });
+
     const updatedItem = {
       ...item,
       id: encodeObjectId(item._id), 
