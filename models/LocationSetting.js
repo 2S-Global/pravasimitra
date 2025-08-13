d2fcbcceff7b823d81e1e82066dd5185eaefa668
souvik-03-sd <souvik.2sglobal@gmail.com>
2025-08-13 16:29:23 +0530

    update

--- models/LocationSetting.js
+++ models/LocationSetting.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const LocationSettingSchema = new mongoose.Schema(
   {
     userId: {
-      type: String,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
       required: true,
     },
     currentCountry: {
