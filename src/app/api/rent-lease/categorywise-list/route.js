06b1e24008bb26a076110b04312826478dfd82d9
souvik-03-sd <souvik.2sglobal@gmail.com>
2025-08-19 17:15:31 +0530

    update

--- src/app/api/rent-lease/categorywise-list/route.js
+++ src/app/api/rent-lease/categorywise-list/route.js
@@ -73,7 +73,7 @@ export const GET = withAuth(async (req,user) => {
 
     // Filter: exclude products posted by this user & match city & country
     query.createdBy = { $ne: userId };
-    query.city = location.currentCity;
+    // query.city = location.currentCity;
     query.country = location.currentCountry;
 
     const items = await RoomItem.find(query)
